44238bbdba344730fb3d7a5646eb2b8133e5b9a9
marcely <arrudamarcely460@gmail.com>
2024-03-25 10:06:23 -0400

    quetão3

---
+++ questao3.js
@@ -0,0 +1,10 @@
+/*
+Questão 3: Como você pode concatenar duas strings em JavaScript?
+*/
+
+let nome = "nome1"
+let sobrenome = "nome2"
+let resultado;
+
+resultado = nome +" "+ sobrenome;
+console.log(resultado);
